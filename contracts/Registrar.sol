8cf11c73783bd2dbf6ee9e499ad49ebe358e8084
David Terry <me@xwvvvvwx.com>
2021-02-04 17:23:32 +0100

    registrar: introduce commit / reveal scheme
    This prevents an attack where a malicious actor could scan the mempool
    and front run name registration before charging the original registrant
    an extra fee.

--- contracts/Registrar.sol
+++ contracts/Registrar.sol
@@ -6,6 +6,35 @@ import "@ensdomains/ens/contracts/ENS.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
+// commitments are kept in a seperate contract to allow the state to be reused
+// between different versions of the registrar
+contract Commitments {
+    address public owner;
+    modifier auth { require(msg.sender == owner, "Commitments: unauthorized"); _; }
+    event SetOwner(address usr);
+
+    mapping(bytes32 => uint) public expires;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setOwner(address usr) external auth {
+        owner = usr;
+        emit SetOwner(usr);
+    }
+
+    function commit(bytes32 commitment, uint deadline) external auth {
+        expires[commitment] = deadline;
+    }
+
+    function mkCommitment(
+        string calldata name, address owner, uint salt
+    ) external pure returns (bytes32) {
+        return keccak256(abi.encodePacked(name, owner, salt));
+    }
+}
+
 contract Registrar {
 
     // --- DATA ---
@@ -16,6 +45,9 @@ contract Registrar {
     /// The Radicle ERC20 token.
     ERC20Burnable public immutable rad;
 
+    /// The commitment storage contract
+    Commitments public immutable commitments = new Commitments();
+
     /// The namehash of the `eth` TLD in the ENS registry, eg. namehash("eth").
     bytes32 public constant ethNode = keccak256(abi.encodePacked(bytes32(0), keccak256("eth")));
 
@@ -25,13 +57,19 @@ contract Registrar {
     /// The token ID for the node in the `eth` TLD, eg. sha256("radicle").
     uint256 public constant tokenId = uint256(keccak256("radicle"));
 
+    /// The maximum time between commiting to a name and registering the name
+    uint256 public commitmentTimeout = 1 days;
+
     /// Registration fee in *Radicle* (uRads).
-    uint256 public registrationFee = 1e18;
+    uint256 public fee = 1e18;
 
     // --- LOGS ---
 
     /// @notice A name was registered.
     event NameRegistered(string indexed name, bytes32 indexed label, address indexed owner);
+
+    /// @notice A commitment was made
+    event CommitmentMade(bytes32 commitment, uint deadline);
 
     /// @notice The contract admin was changed
     event AdminChanged(address newAdmin);
@@ -47,6 +85,15 @@ contract Registrar {
 
     /// @notice The registration fee was changed
     event TTLChanged(uint64 amt);
+
+    /// @notice The commitment timeout was changed
+    event CommitmentTimeoutChanged(uint256 amt);
+
+    // -- MATH --
+
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x, "Registrar: overflow during addition");
+    }
 
     // --- AUTH ---
 
@@ -73,21 +120,36 @@ contract Registrar {
 
     // --- USER FACING METHODS ---
 
+    /// Commit to a future name registration
+    function commit(bytes32 commitment) external {
+        uint deadline = add(block.timestamp, commitmentTimeout);
+        require(commitments.expires(commitment) == 0, "Registrar::commit: already commited");
+
+        rad.burnFrom(msg.sender, fee);
+        commitments.commit(commitment, deadline);
+
+        emit CommitmentMade(commitment, deadline);
+    }
+
     /// Register a subdomain (with the default resolver and ttl)
-    function register(string memory name, address owner) public {
-        register(name, owner, ens.resolver(radNode), 0);
+    function register(string calldata name, address owner, uint salt) external {
+        register(name, owner, salt, ens.resolver(radNode), 0);
     }
 
     /// Register a subdomain (with a custom resolver and ttl)
-    function register(string memory name, address owner, address resolver, uint64 ttl) public {
-        uint256 fee   = registrationFee;
+    function register(
+        string memory name, address owner, uint salt, address resolver, uint64 ttl
+    ) public {
         bytes32 label = keccak256(bytes(name));
+        bytes32 commitment = commitments.mkCommitment(name, owner, salt);
+        uint256 expiry = commitments.expires(commitment);
 
+        require(expiry != 0, "Registrar::register: must commit before registration");
+        require(expiry > block.timestamp, "Registrar::register: commitment expired");
         require(rad.balanceOf(msg.sender) >= fee, "Registrar::register: insufficient rad balance");
         require(available(name), "Registrar::register: name has already been registered");
         require(valid(name), "Registrar::register: invalid name");
 
-        rad.burnFrom(msg.sender, fee);
         ens.setSubnodeRecord(radNode, label, owner, resolver, ttl);
 
         emit NameRegistered(name, label, owner);
@@ -119,11 +181,18 @@ contract Registrar {
 
         ens.setOwner(radNode, newOwner);
         ethRegistrar.transferFrom(address(this), newOwner, tokenId);
+        commitments.setOwner(newOwner);
 
         emit DomainOwnershipChanged(newOwner);
     }
 
-    /// Set a new resolver for radicle.eth
+    /// Set the commitment timeout
+    function setCommitmentTimeout(uint256 amt) public adminOnly {
+        commitmentTimeout = amt;
+        emit CommitmentTimeoutChanged(amt);
+    }
+
+    /// Set a new resolver for radicle.eth.
     function setDomainResolver(address resolver) public adminOnly {
         ens.setResolver(radNode, resolver);
         emit ResolverChanged(resolver);
@@ -137,7 +206,7 @@ contract Registrar {
 
     /// Set a new registration fee
     function setRegistrationFee(uint256 amt) public adminOnly {
-        registrationFee = amt;
+        fee = amt;
         emit RegistrationFeeChanged(amt);
     }
 
